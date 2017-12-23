5b5ea26e88373ca2bd331ab016a22bd32097f027
Lam Nguyen <lnguyen@netsuite.com>
2017-12-23 00:53:46 -0600

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected!');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5a3df5a9eaa1e9211ada5488')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Lahm'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    // db.close();
+});
